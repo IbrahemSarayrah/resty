a2153454033835789f6b1710f828f3356bc0692c
IbrahemSarayrah <ibrahem.sarayrah@gmail.com>
2021-09-20 15:53:17 +0300

    update

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,33 +1,60 @@
 
 import './form.scss';
+import { useState } from "react";
 
-function Form (props) {
+function Form(props) {
 
- const handleSubmit = e => {
+  const [method, setMethod] = useState('get');
+  const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon');
+  const [showTextArea, setshowTextArea] = useState(false);
+
+  // https://pokeapi.co/api/v2/pokemon
+
+  const handleSubmit = e => {
     e.preventDefault();
     const formData = {
-      method:'GET',
-      url: 'https://pokeapi.co/api/v2/pokemon',
+      method: method,
+      url: url,
     };
     props.handleApiCall(formData);
   }
-    return (
-      <>
-        <form onSubmit={handleSubmit}>
-          <label >
-            <span>URL: </span>
-            <input name='url' type='text' />
-            <button type="submit">GO!</button>
-          </label>
-          <label className="methods">
-            <span id="get">GET</span>
-            <span id="post">POST</span>
-            <span id="put">PUT</span>
-            <span id="delete">DELETE</span>
-          </label>
-        </form>
-      </>
-    );
+
+  const handleMethod = (e)=>{
+    setMethod(e.target.id);
+    setshowTextArea(false);
+  }
+
+  const handleUrl = (e)=> {
+    setUrl(e.target.value)
+  }
+
+  const textAreaHandler = (e) => {
+    setshowTextArea(true);
+    setMethod(e.target.id);
+  };
+
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label >
+          <span>URL: </span>
+          <input name='url' type='text' onChange={handleUrl} />
+          <button type="submit">GO!</button>
+        </label>
+        <label className="methods">
+          <span id="get" onClick={handleMethod}>GET</span>
+          <span id="post" onClick={textAreaHandler}>POST</span>
+          <span id="put" onClick={textAreaHandler}>PUT</span>
+          <span id="delete" onClick={textAreaHandler}>DELETE</span>
+        </label>
+        {showTextArea && (
+          <textarea rows="20" cols="45"></textarea>
+        )
+        }
+      </form>
+    </>
+  );
 }
 
 export default Form;
--- src/components/results/index.js
+++ src/components/results/index.js
@@ -1,9 +1,12 @@
 import './results.scss'
+import JSONPretty from 'react-json-pretty';
+import 'react-json-pretty/themes/monikai.css';
 
 function Results(props) {
     return (
       <section>
-        <pre>{props.data ? JSON.stringify(props.data, undefined, 2) : null}</pre>
+        {/* <pre>{props.data ? JSON.stringify(props.data, undefined, 2) : null}</pre> */}
+        <JSONPretty id="json-pretty" data={props.data ? JSON.stringify(props.data, undefined, 2) : null}></JSONPretty>
       </section>
     );
 }
